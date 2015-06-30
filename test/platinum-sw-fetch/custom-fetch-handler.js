a7d8fa7bbfd9b6f7c080e9ba23317942fb95737f
Jeff Posnick <jeffy@google.com>
2015-06-30 13:16:26 -0400

    Test for cross-origin fetch handlers.

--- test/platinum-sw-fetch/custom-fetch-handler.js
+++ test/platinum-sw-fetch/custom-fetch-handler.js
@@ -1,6 +1,13 @@
-self.customFetchHandler = function(request) {
+self.custom204FetchHandler = function(request) {
   return new Response('', {
     status: 204,
+    statusText: 'Via customFetchHandler'
+  });
+};
+
+self.custom410FetchHandler = function(request) {
+  return new Response('', {
+    status: 410,
     statusText: 'Via customFetchHandler'
   });
 };
