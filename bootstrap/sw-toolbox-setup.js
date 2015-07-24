334f12fa52c5fb19bfa19970094443ecb24346d0
Jeff Posnick <jeffy@google.com>
2015-07-24 15:37:56 -0400

    New <platinum-sw-offline-analytics> element

--- bootstrap/sw-toolbox-setup.js
+++ bootstrap/sw-toolbox-setup.js
@@ -8,35 +8,38 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-var swToolboxURL = new URL('../sw-toolbox/sw-toolbox.js', params.get('baseURI')).href;
-importScripts(swToolboxURL);
-
-if (params.has('cacheId')) {
-  toolbox.options.cacheName = params.get('cacheId') + '$$$' + self.registration.scope;
-}
-
-if (params.has('defaultCacheStrategy')) {
-  var strategy = params.get('defaultCacheStrategy');
-  toolbox.router.default = toolbox[strategy] || self[strategy];
-}
-
-if (params.has('precache')) {
-  toolbox.precache(params.get('precache'));
-}
-
-if (params.has('route')) {
-  var setsOfRouteParams = params.get('route');
-  while (setsOfRouteParams.length > 0) {
-    var routeParams = setsOfRouteParams.splice(0, 3);
-    var originParam;
-    if (routeParams[2]) {
-      originParam = {origin: new RegExp(routeParams[2])};
-    }
-    var handler = toolbox[routeParams[1]] || self[routeParams[1]];
-    if (typeof handler === 'function') {
-      toolbox.router.get(routeParams[0], handler, originParam);
-    } else {
-      console.error('Unable to register sw-toolbox route: ', routeParams);
+(function(global) {
+  var swToolboxURL = new URL('../sw-toolbox/sw-toolbox.js', global.params.get('baseURI')).href;
+  importScripts(swToolboxURL);
+  
+  if (global.params.has('cacheId')) {
+    global.toolbox.options.cacheName = global.params.get('cacheId') + '$$$' +
+      global.registration.scope;
+  }
+  
+  if (global.params.has('defaultCacheStrategy')) {
+    var strategy = global.params.get('defaultCacheStrategy');
+    global.toolbox.router.default = global.toolbox[strategy] || global[strategy];
+  }
+  
+  if (global.params.has('precache')) {
+    global.toolbox.precache(global.params.get('precache'));
+  }
+  
+  if (global.params.has('route')) {
+    var setsOfRouteParams = global.params.get('route');
+    while (setsOfRouteParams.length > 0) {
+      var routeParams = setsOfRouteParams.splice(0, 3);
+      var originParam;
+      if (routeParams[2]) {
+        originParam = {origin: new RegExp(routeParams[2])};
+      }
+      var handler = global.toolbox[routeParams[1]] || global[routeParams[1]];
+      if (typeof handler === 'function') {
+        global.toolbox.router.get(routeParams[0], handler, originParam);
+      } else {
+        console.error('Unable to register sw-toolbox route: ', routeParams);
+      }
     }
   }
-}
+})(self);
