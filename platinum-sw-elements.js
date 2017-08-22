ced813a371ab210dc307ad0ea0eb8638e826421e
Peter Burns <rictic@google.com>
2017-08-22 01:58:48 -0700

    Automatic polymer-modulizer conversion of a2380085b149c0025d38df0d0f717ebaaff95338

---
+++ platinum-sw-elements.js
@@ -0,0 +1,5 @@
+import './platinum-sw-cache.js';
+import './platinum-sw-fetch.js';
+import './platinum-sw-import-script.js';
+import './platinum-sw-offline-analytics.js';
+import './platinum-sw-register.js';
