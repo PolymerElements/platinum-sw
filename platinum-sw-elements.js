c96fc744916c4714fe1ff7b814e76c21f72ca229
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:38 -0700

    auto-converted by polymer-modulizer

--- platinum-sw-elements.js
+++ platinum-sw-elements.js
@@ -1,4 +1,10 @@
-<!--
+import './platinum-sw-cache.js';
+import './platinum-sw-fetch.js';
+import './platinum-sw-import-script.js';
+import './platinum-sw-offline-analytics.js';
+import './platinum-sw-register.js';
+
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,11 +12,11 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<!-- Facilitates importing the full collection of platinum-sw elements. -->
-<link rel="import" href="platinum-sw-cache.html">
-<link rel="import" href="platinum-sw-fetch.html">
-<link rel="import" href="platinum-sw-import-script.html">
-<link rel="import" href="platinum-sw-offline-analytics.html">
-<link rel="import" href="platinum-sw-register.html">
+*/
+/* Facilitates importing the full collection of platinum-sw elements. */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;
